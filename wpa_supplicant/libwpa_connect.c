720533aab0d359f943f9461d6f48b8217636de2e
dnagis <vincent.achard@gmail.com>
2021-11-19 11:13:19 +0100

    mac addresses requester: checke si connue, et request avec variable

--- wpa_supplicant/libwpa_connect.c
+++ wpa_supplicant/libwpa_connect.c
@@ -19,9 +19,33 @@
  
  
 #include <string.h>
+#include <stdbool.h>
 
 #include "includes.h"
 #include "common/wpa_ctrl.h"
+
+
+/**
+ * Compare addr avec une liste d'adresses connues, return true si au moins une occurence
+ * 
+ */
+bool chkmacaddr(char *addr)
+{
+	char *known_addrs[] = {
+	"1a:f0:e4:11:ef:ba", //redmi note 4
+	"d6:c9:4b:81:c2:73", //?
+	"ENDOFARRAY"};
+	int i=0;
+		
+	while (strcmp(known_addrs[i]	, "ENDOFARRAY")) {
+		if (strstr(addr, known_addrs[i])) return true;
+		i++;
+	};
+	
+	return false;
+}
+
+
 
 int main(int argc, char *argv[])
 {
@@ -58,16 +82,36 @@ int main(int argc, char *argv[])
 			
 			printf("event: len=%d , %s \n",len,buf);
 			
-			if (strstr(buf, "P2P-GO-NEG-REQUEST d6:c9:4b:81:c2:73")) {
+			//On veut gérer qq chose qui ressemble à:
+			//<3>P2P-GO-NEG-REQUEST 1a:f0:e4:11:ef:ba dev_passwd_id=4 go_intent=64:11:ef:ba bssid=1a:f0:e4:11:ef:ba unknown-networkonfig_methods=0x188 dev_capab=0x25 group_capab=0x0 new=1 
+			if (strstr(buf, "P2P-GO-NEG-REQUEST")) {
+				printf("on a Rx: P2P-GO-NEG-REQUEST\n");
+				
+				//Récupération de l'addr mac du requester
+				char * raddr; //requester mac address
+				char * token; //utilisation de strtok est un peu spéciale
+				token = strtok(buf, " "); //à ce stade token = <3>P2P-GO-NEG-REQUEST
+				raddr = strtok(NULL, " "); //oui c'est hyper chelou la manière d'avancer dans buf... là on est au 2ème token
+				printf("requester mac addr=%s\n", raddr);
+
 
 				
-				printf("On a une demande de connexion\n");
+				//on checke si addr du requester connue, fonction perso
+				if (chkmacaddr(raddr)){
+					 printf("chkmacaddr a return true donc addresse: %s connue\n", raddr);
 				
-				
-				//wpa_ctrl_request(ctrl, "P2P_CONNECT d6:c9:4b:81:c2:73 PBC GO_INTENT=0", 45, buf, &len, NULL); --> FAIL-INVALID-PIN
-				len = sizeof(buf) - 1;
-				wpa_ctrl_request(ctrl, "P2P_CONNECT d6:c9:4b:81:c2:73 pbc go_intent=0", 45, buf, &len, NULL);
-				printf("reponse: len=%d , %s \n",len,buf);
+					//On répond à la demande de connexion:
+					//Syntaxe pour une commande à passer(la casse est... particulière):
+					//wpa_ctrl_request(ctrl, "P2P_CONNECT d6:c9:4b:81:c2:73 PBC GO_INTENT=0", 45, buf, &len, NULL); --> donne FAIL-INVALID-PIN il faut minuscules mais ONLY après la macaddr!
+					char cmd[45] = "P2P_CONNECT ";
+					strcat(cmd, raddr);
+					strcat(cmd, " pbc go_intent=0");
+					printf("on va envoyer la commande: %s\n", cmd);
+					len = sizeof(buf) - 1;
+					wpa_ctrl_request(ctrl, cmd, 45, buf, &len, NULL);
+					printf("reponse: len=%d , %s \n",len,buf);
+					
+					}					
 				}
 			}
 		

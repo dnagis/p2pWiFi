7a80d0595ffda0fc67b7f3987a17c2eda807e7b4
dnagis <vincent.achard@gmail.com>
2021-12-04 11:52:41 +0100

    mantras et travail dbus

--- wpa_supplicant/dbus/simple_loop.py
+++ wpa_supplicant/dbus/simple_loop.py
@@ -1,3 +1,8 @@
+#loop pour attendre des events venant de wpas. Et qui ne fasse pas tourner 100% du CPU
+#comme les exemples trouvés dans les sources wpa_supplicant (wpa_supplicant-2.9/wpa_supplicant/examples/p2p/)
+#dbus-python-1.2.18/examples/example-service.py
+
+
 #!/usr/bin/env python3
 
 

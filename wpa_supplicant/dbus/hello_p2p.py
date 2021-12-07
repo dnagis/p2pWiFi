c797707b45db6d2fb5c90315ba1f59b7d4f37df8
dnagis <vincent.achard@gmail.com>
2021-12-07 10:17:20 +0100

    bidouillages dbus-python, get_object, interfaces....

---
+++ wpa_supplicant/dbus/hello_p2p.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+
+#Hello_p2p
+#Je pars de hello_wpas et j'essaie d'avoir le code le plus simple possible pour accéder au P2P et lancer p2p_find
+
+#https://dbus.freedesktop.org/doc/dbus-python/tutorial.html
+#p2p_find.py
+
+
+
+import dbus
+
+
+
+
+bus = dbus.SystemBus()
+
+#Récupération de wpas: un proxy vers /fi/w1/wpa_supplicant1, de type: dbus.proxies.ProxyObject
+wpas = bus.get_object('fi.w1.wpa_supplicant1','/fi/w1/wpa_supplicant1') 
+
+#Appel de la methode GetInterface() définie dans l'API dbus de wpa_supplicant (https://w1.fi/wpa_supplicant/devel/dbus.html#dbus_main)
+wlan0_path = wpas.GetInterface('wlan0', dbus_interface='fi.w1.wpa_supplicant1')
+
+
+print(wlan0_path)
+
+
+#Je récupère un proxy vers wlan0, en utilisant l'objet proxy wpas
+wlan0_object = bus.get_object('fi.w1.wpa_supplicant1', wlan0_path)
+
+#Je récupère une objet de type dbus.Interface qui me permettra d'appeler les methodes dessus
+p2p_interface = dbus.Interface(wlan0_object, dbus_interface='fi.w1.wpa_supplicant1.Interface.P2PDevice')
+
+
+
+#Appel de Find(), possible de vérifier le fonctionnement avec une autre fenêtre wpa_cli -i p2p-dev-wlan0 
+P2PFindDict = dbus.Dictionary({'Timeout':int(30)})
+p2p_interface.Find(P2PFindDict)
+
+
+
+
+
--- wpa_supplicant/dbus/hello_wpas.py
+++ wpa_supplicant/dbus/hello_wpas.py
@@ -2,26 +2,31 @@
 
 
 #Hello_wpas
-#Bricoler avec l'api dbus de wpa_supplicant fi.w1.wpa_supplicant1 (https://w1.fi/wpa_supplicant/devel/dbus.html)
 
-#en utilisant les recettes dbus-python
-#https://dbus.freedesktop.org/doc/dbus-python/tutorial.html
+#Appel le plus simple possible de wpa_supplicant
+#en utilisant dbus-python: https://dbus.freedesktop.org/doc/dbus-python/tutorial.html
 
-#Ici le plus simple: appeler la methode GetInterface de fi.w1.wpa_supplicant1
+#api dbus de wpa_supplicant fi.w1.wpa_supplicant1 (https://w1.fi/wpa_supplicant/devel/dbus.html)
+
+
+
+#appel de la methode GetInterface() définie dans fi.w1.wpa_supplicant1 (https://w1.fi/wpa_supplicant/devel/dbus.html)
 
 
 
 import dbus
 
+
+
 bus = dbus.SystemBus()
 
-proxy = bus.get_object('fi.w1.wpa_supplicant1','/fi/w1/wpa_supplicant1') # proxy is a dbus.proxies.ProxyObject
+proxy = bus.get_object('fi.w1.wpa_supplicant1','/fi/w1/wpa_supplicant1') # proxy est un proxy vers wpas: de type: dbus.proxies.ProxyObject
 
 #je veux appeler la method GetInterface ( s : ifname ) –> o : interface "Returns a D-Bus path to an object related to an interface which wpa_supplicant already controls."
 # Arguments s : ifname Name of the network interface, e.g., wlan0
 # Returns o : interface A D-Bus path to an object representing an interface
 
-path = proxy.GetInterface('wlan0', dbus_interface='fi.w1.wpa_supplicant1') #si dbus_interface= en premier SyntaxError: positional argument follows keyword argument
+path = proxy.GetInterface('wlan0', dbus_interface='fi.w1.wpa_supplicant1') #si dbus_interface='...' en premier --> SyntaxError: positional argument follows keyword argument
 print(path)
 
 

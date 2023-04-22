6cacaf0cfc3d539072d4830a172503c66ae48778
dnagis <vincent.achard@gmail.com>
2023-04-22 14:00:56 +0200

    meilleure compréhension de la décomposition du variant avec l arg2 de g_variant_get

--- wpa_supplicant/dbus/g_variant-access-data.c
+++ wpa_supplicant/dbus/g_variant-access-data.c
@@ -1,6 +1,15 @@
-//gcc `pkg-config --cflags --libs glib-2.0 gio-2.0` -o g_variant-access-data g_variant-access-data.c
+/**
+ * Accéder au données récupérées dans un variant
+ * type (oa{sv})
+ * 
+ * https://docs.gtk.org/glib/gvariant-format-strings.html#dictionaries
+ * 
+ * 
+ * gcc `pkg-config --cflags --libs glib-2.0 gio-2.0` -o g_variant-access-data g_variant-access-data.c
+ * 
+ * */
 
-//https://docs.gtk.org/glib/gvariant-format-strings.html#dictionaries
+
 
 #include <gio/gio.h>
 
@@ -8,31 +17,42 @@ int
 main (void)
 {
 
-	GVariant *data;
+	GVariant *data, *params, *p_brightness;
 	gint value = 1;
 	gint max = 3;
+	gint p_max;	
+
 	
-	/* type (oa{sv}) */
+	/* Création d'un variant type (oa{sv}) */
 	data = g_variant_new_parsed ("(%o, {'value': <%i>, 'max': <%i>})", "/object/path", value, max);
 	                             
 	g_print ("g_variant_get_type_string sur le variant data: %s\n", g_variant_get_type_string(data));
 	g_print ("g_variant_print sur le variant data: %s\n", g_variant_print (data, TRUE)); 
-
-	GVariant *params;
-	GVariant *p_brightness;
-	gchar *obj;
-	gint p_max;
 	
-	g_variant_get (data, "(o@a{?*})", &obj, &params);
 	
-	g_print ("object_path: %s\n", obj);
+	/**
+	 * coeur du fonctionnement: dissocie l'objet et l'array de dict (ce qui m interesse le plus)
+	 * 
+	 * g_variant_get (data, "(o@a{sv})", &obj, &params); //si tu veux récupérer un gchar *obj qui te donnera l object path (printable avec g_print ("object_path: %s\n", obj);)
+	 * 
+	 * g_variant_get (data, "(o@a{?*})", &obj, &params); //utilisé dans l exemple source https://docs.gtk.org/glib/gvariant-format-strings.html#dictionaries dont le but est d extraire un nested dict (oa{sa{sv})
+	 * 
+	 * 
+	**/
+	g_variant_get (data, "(o@a{sv})", NULL, &params); 
+	
+	
+	
+	
 	g_print ("g_variant_print sur le variant params: %s\n", g_variant_print (params, TRUE));
 	
-	g_variant_lookup (params, "value", "i", &p_max);
-	g_print ("value: %d\n", p_max);
+	g_variant_lookup (params, "max", "i", &p_max);
+	g_print ("max: %d\n", p_max);
 	
 	
-	/** Pour accéder à des nested dicts type (oa{sa{sv}) cf. https://docs.gtk.org/glib/gvariant-format-strings.html#dictionaries
+	
+	
+	/** Pour accéder à des nested dicts type (oa{sa{sv})
 	 * 
 	 * 
 	p_brightness = g_variant_lookup_value (params, "brightness", G_VARIANT_TYPE_VARDICT);

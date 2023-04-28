9066445ef6eabed01282465e87ea05dd013eda13
dnagis <vincent.achard@gmail.com>
2023-04-28 09:39:51 +0200

    appel connect fonctionnel

--- wpa_supplicant/dbus/g_p2p_connect.c
+++ wpa_supplicant/dbus/g_p2p_connect.c
@@ -1,9 +1,8 @@
 /**
- * Connexion p2p comme avec p2p_connect.py
+ * Connexion p2p si device name matche
  * 
  * gcc g_p2p_connect.c -o g_p2p_connect `pkg-config --cflags --libs glib-2.0 gio-2.0`
- * 
- * 
+ *  
  */
 
 #include <gio/gio.h>
@@ -19,44 +18,32 @@ GDBusProxy *proxy = NULL;
  * 
  * https://w1.fi/wpa_supplicant/devel/dbus.html#dbus_p2pdevice
  * 
- * NetworkManager --> nm-supplicant-interface.c ligne 2824 nm_supplicant_interface_p2p_connect()
+ * Inspiration de NetworkManager --> nm-supplicant-interface.c ligne 2824 nm_supplicant_interface_p2p_connect()
  * 
  * 
  **/ 
 
 
 static void connect(gchar *peer) {
-	
-	
+
 	GVariantBuilder builder;
 	GError *error = NULL;
 	
 	g_print ("Fonction connect() pour peer = %s\n", peer);
-	
-	
-	
-	//g_print ("Dans connect() g_variant_get_type_string sur le GVariant peer: %s\n", g_variant_get_type_string(peer));
-	
+
+	/**
+	 * Construction du dict contenant les paramètres à passer à Connect
+	 * 
+	 * **/
     g_variant_builder_init(&builder, G_VARIANT_TYPE_VARDICT);
 
     g_variant_builder_add(&builder, "{sv}", "wps_method", g_variant_new_string("pbc"));
     g_variant_builder_add(&builder, "{sv}", "join", g_variant_new_boolean(TRUE));    
-    
-    
-    //Bloque au runtime ici
-     
-    
-    g_variant_builder_add(&builder, "{sv}", "peer", g_variant_new_object_path(peer)); //il faut gchar* pour avoir qq chose
-    //g_variant_builder_add(&builder, "{sv}", "peer", peer);
-	
+    g_variant_builder_add(&builder, "{sv}", "persistent", g_variant_new_boolean(TRUE));
+    //Attention: j'ai déjà essayé de passer un GVariant* --> g_variant_builder_add() bloque sans erreur ni segfault!    
+    g_variant_builder_add(&builder, "{sv}", "peer", g_variant_new_object_path(peer)); 
 
-
-
-
-    //g_variant_builder_add(&builder, "{sv}", "persistent", g_variant_new_boolean(TRUE));
     //g_variant_builder_add(&builder, "{sv}", "go_intent", g_variant_new_int32(7));
-
-	g_print ("on est avant le lancement de g_dbus_proxy_call_sync() pour Connect\n");
 
 	g_dbus_proxy_call_sync (proxy,
 							"Connect",
@@ -65,8 +52,6 @@ static void connect(gchar *peer) {
 							-1,
 							NULL,
 							&error);
-							
-    g_print ("on est apres le lancement de g_dbus_proxy_call_sync() pour Connect\n");
 							
 	if (error != NULL) g_print("Erreur g_dbus_proxy_call_sync pour Connect: %s\n", error->message);
 	
@@ -82,45 +67,28 @@ static void on_signal (GDBusProxy *proxy,
 	
 	GVariant *devInfoDict;
 	gchar *devName;
-	//GVariant *peer;  
-	gchar *peer;
+	gchar *peer; //Attention je sais que tu auras envie de remplacer ça par GVariant * mais déjà essayé +++
 	
     g_print("on_signal() signal_name=%s\n", signal_name);
     
     //g_print ("g_variant_get_type_string sur le GVariant params: %s\n", g_variant_get_type_string(params));
     //g_print ("g_variant_print sur le GVariant params: %s\n", g_variant_print (params, TRUE)); 
     
-    //Le device name ('RPi4') n'est présent que quand le signal = DeviceFoundProperties
+    
     if (g_strcmp0(signal_name, "DeviceFoundProperties") == 0) {
-        g_print("signal_name = DeviceFoundProperties\n");
         
-        g_print("on cherche %s\n", dev_searched);
-        
-        //DeviceName
+        //Passer les informations reçues avec le signal dans des variables locales
         g_variant_get (params, "(o@a{sv})", &peer, &devInfoDict);
+        
+        //Device name pour match et conditionner lancement connect
         g_variant_lookup (devInfoDict, "DeviceName", "s", &devName);    
-		g_print ("DeviceName: %s\n", devName); 
-		
-		//Object Path https://docs.gtk.org/glib/gvariant-format-strings.html à l'exemple strings
-		if (peer == NULL) {
-			g_print ("peer est NULL\n");
-				} else {
-			g_print ("peer = %s\n", peer);
-			}
-		
-		
-		//g_print ("Dans on_signal() g_variant_get_type_string sur le GVariant peer: %s\n", g_variant_get_type_string(peer)); //SegFault
-
-
-				
+		g_print ("on cherche %s, on vient de trouver: %s\n",dev_searched, devName); 
+					
 		if (g_strcmp0(devName, dev_searched) == 0) {
-			g_print ("On a trouve: %s on lance un connect...\n", dev_searched); 
+			//g_print ("On a trouve: %s on lance un connect...\n", dev_searched); 
 			connect(peer);
 			}
-		
-        
-        
-       
+      
     } 
 
 }
@@ -151,10 +119,10 @@ int main() {
 	 * Appel de Find() sur fi.w1.wpa_supplicant1.Interface.P2PDevice
 	 * https://w1.fi/wpa_supplicant/devel/dbus.html#dbus_p2pdevice
 	 * 
-	 * Construction des arguments pour la méthode Find: network manager: nm-supplicant-interface.c
+	 * Construction des arguments pour la méthode Find: inspiration: network manager: nm-supplicant-interface.c
 	 * */
 
-	guint timeout = 60; //Timeout (s) au terme --> P2P-FIND-STOPPED
+	guint timeout = 120; //En secondes. Au terme --> P2P-FIND-STOPPED
 	GVariantBuilder methodParms;
 		
 	g_variant_builder_init(&methodParms, G_VARIANT_TYPE_VARDICT);
